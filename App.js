ae24e3500a52c1808d68ffded6ae4e1f2d6ad733
shadi Shnaker <shadish@ac.sce.ac.il>
2020-09-15 07:00:45 +0300

    add firebase

--- App.js
+++ App.js
@@ -5,6 +5,20 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import SignInScreen from "./src/screens/SignInScreen";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH_4XXf5iLpWqYjkAmViFkCsOV-VjqQj0",
+  authDomain: "allinfit-caa00.firebaseapp.com",
+  databaseURL: "https://allinfit-caa00.firebaseio.com",
+  projectId: "allinfit-caa00",
+  storageBucket: "allinfit-caa00.appspot.com",
+  messagingSenderId: "840905150664",
+  appId: "1:840905150664:web:924a888803bf26e4bc55fd",
+  measurementId: "G-SCYPMCDPCP",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const navigator = createStackNavigator(
   {
