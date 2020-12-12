f3c0e5bfa49c289177ddac517d385d869e3936da
shadi Shnaker <shadishnaker@gmail.com>
2020-12-12 18:08:25 +0200

    Bmi Component

---
+++ src/components/BMI.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+const BMIMet = require("../../assets/BMI.png");
+
+const BMI = (props) => {
+  const [advice, setAdvice] = useState("");
+  const [result, setResult] = useState("");
+
+  const AdviceMessage = () => {
+    if (result >= 15 && result <= 18.5) {
+      setAdvice("you are uderweight! you should gain weight.");
+    }
+    if (result > 18.5 && result <= 25) {
+      setAdvice("your weight is normal! you should maintain your weight.");
+    }
+    if (result > 25 && result <= 30) {
+      setAdvice("you are overweight! you should loss weight.");
+    }
+    if (result > 30 && result <= 40) {
+      setAdvice("your weight is Obese! you must loss weight.");
+    }
+  };
+
+  useEffect(() => {
+    setResult(Math.trunc((props.weight / props.height / props.height) * 10000));
+
+    AdviceMessage();
+  });
+  return (
+    <View style={styles.Container}>
+      <Text
+        style={{
+          alignSelf: "center",
+          paddingTop: 10,
+          fontSize: 19,
+          fontWeight: "bold",
+        }}
+      >
+        {result}
+      </Text>
+      <Text style={{ alignSelf: "center", paddingTop: 10, color: "#86789E" }}>
+        {advice}
+      </Text>
+      <Image style={styles.image} source={BMIMet} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 0.2,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    borderColor: "#772E92",
+    borderWidth: 1,
+    marginHorizontal: 15,
+    marginTop: 40,
+    paddingBottom: 10,
+  },
+
+  image: {
+    alignSelf: "center",
+    marginTop: 20,
+  },
+});
+
+export default BMI;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,68 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, ImageBackground, LogBox } from "react-native";
+import BMI from "../components/BMI";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
-const HomeScreen = () => {
+const backGround = require("../../assets/HOME.jpg");
+
+const HomeScreen = ({ navigation }) => {
+  LogBox.ignoreLogs(["Setting a timer"]);
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+  const [currentUser, setCurrentUser] = useState("");
+  const [name, setName] = useState("");
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+
+  const getUserData = async () => {
+    const user = navigation.getParam("user", " ");
+
+    const currentUserData = await firebase
+      .database()
+      .ref("users/" + user.uid)
+      .once("value", (snapshot) => {
+        setName(snapshot.val().name);
+        setWeight(snapshot.val().weight);
+        setHeight(snapshot.val().height);
+      });
+
+    setCurrentUser(currentUserData);
+  };
+
+  useEffect(() => {
+    var houres = new Date().getHours();
+
+    if (houres >= "16" || houres < "03") {
+      setWelcomeMessage("GOOD EVNING");
+    } else if (houres >= "10" || houres < "16") {
+      setWelcomeMessage("GOOD AFTERNOON");
+    } else {
+      setWelcomeMessage("GOOD MORNING");
+    }
+
+    getUserData();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={{ paddingTop: 70 }}>Home Screen</Text>
+      <ImageBackground source={backGround} style={styles.image}>
+        <Text
+          style={{
+            paddingTop: 55,
+            paddingLeft: 20,
+            fontSize: 11,
+            color: "#C1B8D0",
+          }}
+        >
+          {welcomeMessage}
+        </Text>
+
+        <Text style={{ paddingLeft: 20, fontSize: 25, color: "#FFF" }}>
+          {name}
+        </Text>
+
+        <BMI weight={weight} height={height} />
+      </ImageBackground>
     </View>
   );
 };
@@ -12,7 +70,11 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: "#52677D",
+  },
+
+  image: {
+    flex: 1,
   },
 });
 
